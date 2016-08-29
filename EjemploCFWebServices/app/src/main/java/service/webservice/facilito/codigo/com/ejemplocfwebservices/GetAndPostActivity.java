99cebb986a364fad1ee6cc387043fcdef235e932
aldakur <aldakur@gmail.com>
2016-08-29 15:11:33 +0200

    Limpieza de código en GetAndPostActivity.java y activity_get_and_post.xml

--- EjemploCFWebServices/app/src/main/java/service/webservice/facilito/codigo/com/ejemplocfwebservices/GetAndPostActivity.java
+++ EjemploCFWebServices/app/src/main/java/service/webservice/facilito/codigo/com/ejemplocfwebservices/GetAndPostActivity.java
@@ -18,15 +18,6 @@ public class GetAndPostActivity extends AppCompatActivity {
     TextView textView;
     ProgressBar progressBar;
     String method;
-    //List<MyTask> taskList; //Lista de hilos
-    //List<Usuario> usuarioList;
-
-    // ListView listView;
-    // MyAdapter adapter;
-
-
-    //RecyclerView recyclerView;
-    //UsuariosAdapter adapter;
 
 
     @Override
@@ -41,41 +32,17 @@ public class GetAndPostActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
         progressBar.setVisibility(View.INVISIBLE);
 
-        // listView = (ListView) findViewById(R.id.listview);
-        //recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        //linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        //recyclerView.setLayoutManager(linearLayoutManager);
-
-
-
-
-        // textView.setMovementMethod(new ScrollingMovementMethod()); // android:scrollbars="vertical" kodeari kaso egiteko
-
-        //taskList = new ArrayList<>(); // Inicializamos la lista
-
-
 
         botonGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(isOnLine()){
-                    // pedirDatos("http://www.harrika.net/usuarios.xml");
-                    // pedirDatos("http://maloschistes.com/maloschistes.com/jose/webservice.php"); //JSON sin seguridad
-                    // pedirDatos("http://maloschistes.com/maloschistes.com/jose/s/webservice.php");
-                    // pedirDatos("http://maloschistes.com/maloschistes.com/jose/webserviceI.php"); // Webservices con las imagenes en el apartado Twitter
                     method = "GET";
                     pedirDatos("http://maloschistes.com/maloschistes.com/jose/webservicesend.php"); // Web service enviando GET y POST
 
                 }else{
                     Toast.makeText(getApplicationContext(), "Sin conexión", Toast.LENGTH_SHORT).show();
                 }
-                /*
-                for(int i=0; i<=100; i++){
-                    cargarDatos("Numero: "+i);
-                //}
-                task.execute();*/
-
 
             }
         });
@@ -99,19 +66,6 @@ public class GetAndPostActivity extends AppCompatActivity {
 
     public void cargarDatos(String datos){
         textView.append(datos+"\n"); // Unimos los datos (lo que sería la URL con los parametros GET)
-       /* if(usuarioList != null){
-            for (Usuario usuario: usuarioList) { // foreach
-                textView.append(usuario.getNombre()+"\n");
-
-            }
-        }*/
-        // adapter = new MyAdapter(getApplicationContext(), usuarioList);
-        // listView.setAdapter(adapter);
-
-        //adapter = new UsuariosAdapter(getApplicationContext(), usuarioList);
-        //recyclerView.setAdapter(adapter);
-        //recyclerView.setHasFixedSize(true);
-
 
     }
 
@@ -131,8 +85,6 @@ public class GetAndPostActivity extends AppCompatActivity {
         //task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         //task.execute(uri);
         RequestPackage requestPackage = new RequestPackage();
-        // requestPackage.setMethod("GET");
-        // requestPackage.setMethod("POST");
         requestPackage.setMethod(method);
         requestPackage.setUri(uri);
         requestPackage.setParam("parametro1", "valor1");
@@ -144,40 +96,19 @@ public class GetAndPostActivity extends AppCompatActivity {
 
     }
 
-
-
-    // private class MyTask extends AsyncTask<String, String, String>{
     private class MyTask extends AsyncTask<RequestPackage, String, String> {
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            // cargarDatos("Inicio de carga");
             progressBar.setVisibility(View.VISIBLE);
-
-            /*
-            if(taskList.size() == 0) {
-                progressBar.setVisibility(View.VISIBLE);
-            }
-            taskList.add(this); // Agregando el hilo actual al la lista
-            */
 
         }
 
         @Override
         protected String doInBackground(RequestPackage... params) { // Recibimos un RequestPackage
-                /*
-                for(int i=0; i<=5; i++){
-                    //No podemos cambiar la interface en doInBackground pq es el hilo principal. Por eso podemos usar publishProgress dentro de el
-                    publishProgress("Numero: "+i);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }*/
+
             String content = HttpManager.getData(params[0]); //Cadena de contenido. Sin seguridad
-            // String content = HttpManager.getData(params[0], "pepito", "pepito"); //Cadena de contenido. Con seguridad
             return content;
 
         }
@@ -185,14 +116,6 @@ public class GetAndPostActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) { // result es el resultado de doInBackground
             super.onPostExecute(result);
-            // usuarioList = UsuarioXMLParser.parser(result); PARA PARSEAR EL XML
-
-            /*
-            tasks.remove(this);
-            if(tasks.size() == 0) {
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-            */
 
             if(result == null){
 
@@ -201,23 +124,15 @@ public class GetAndPostActivity extends AppCompatActivity {
                 return;
 
             }
-            //usuarioList = UsuarioJSONParser.parser(result);
+
             cargarDatos(result);
             progressBar.setVisibility(View.INVISIBLE);
-
-            //cargarDatos(result);
-            /*
-            taskList.remove(this);
-            if(taskList.size() == 0) {
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-            */
 
         }
 
         @Override
         protected void onProgressUpdate(String... values) {
-            //cargarDatos(values[0]);
+            // nothing
         }
     }
 
