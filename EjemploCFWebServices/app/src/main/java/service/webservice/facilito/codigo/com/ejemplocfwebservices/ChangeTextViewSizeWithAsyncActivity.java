3862ece1e8d667a0154206d666d42dae0db573df
aldakur <aldakur@gmail.com>
2016-08-29 14:59:02 +0200

    Limpieza de código en ChangeTextViewSizeWithAsyncActivity

--- EjemploCFWebServices/app/src/main/java/service/webservice/facilito/codigo/com/ejemplocfwebservices/ChangeTextViewSizeWithAsyncActivity.java
+++ EjemploCFWebServices/app/src/main/java/service/webservice/facilito/codigo/com/ejemplocfwebservices/ChangeTextViewSizeWithAsyncActivity.java
@@ -1,13 +1,13 @@
 package service.webservice.facilito.codigo.com.ejemplocfwebservices;
 
-import android.content.Intent;
+
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class ChangeTextViewSizeWithAsyncActivity extends AppCompatActivity {
 
@@ -26,13 +26,13 @@ public class ChangeTextViewSizeWithAsyncActivity extends AppCompatActivity {
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new MyAsyncTask().execute(100);
+                new MyAsyncTask().execute(100); // El 100 indica el tamaño maximo del valor del textView
 
             }
         });
     }
 
-    // private class MyTask extends AsyncTask<String, String, String>{
+
     private class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
 
         @Override
@@ -46,9 +46,8 @@ public class ChangeTextViewSizeWithAsyncActivity extends AppCompatActivity {
         protected String doInBackground(Integer... params) { // Recibimos un Integer
             int max = params[0];
 
-                for(int i=1; i < max; i++){
-                    //No podemos cambiar la interface en doInBackground pq es el hilo principal. Por eso podemos usar publishProgress dentro de el
-                    //publishProgress("Numero: "+i);
+                for(int i=1; i <= max; i++){
+                    //No podemos cambiar la interface en doInBackground pq es el hilo principal. Por eso podemos usar publishProgress dentro de el doInBackground
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
@@ -71,7 +70,7 @@ public class ChangeTextViewSizeWithAsyncActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate();
-            int contador = values[0]; // Agarramos el valor que recibimos
+            int contador = values[0]; // Agarramos el valor que recibimos. Al recibir solo uno, agarramos el de la posición cero.
             String texto = "Contador"+contador;
             textView.setText(texto);
             textView.setTextSize(contador);
