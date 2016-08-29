b57c41b2e988c67cfa4d01f6321c2c67bd5ae15c
aldakur <aldakur@gmail.com>
2016-08-29 15:02:25 +0200

    Limpieza de código en DataInListViewActivity

--- EjemploCFWebServices/app/src/main/java/service/webservice/facilito/codigo/com/ejemplocfwebservices/DataInListViewActivity.java
+++ EjemploCFWebServices/app/src/main/java/service/webservice/facilito/codigo/com/ejemplocfwebservices/DataInListViewActivity.java
@@ -6,16 +6,12 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.List;
-
 import service.webservice.facilito.codigo.com.ejemplocfwebservices.Adapters.MyAdapter;
 import service.webservice.facilito.codigo.com.ejemplocfwebservices.POJO.Usuario;
 import service.webservice.facilito.codigo.com.ejemplocfwebservices.Parses.UsuarioJSONParser;
@@ -24,18 +20,11 @@ public class DataInListViewActivity extends AppCompatActivity {
 
 
     Button buttonCargarDatos;
-    TextView textView;
     ProgressBar progressBar;
-    String method;
-    // List<MyTask> taskList; //Lista de hilos
     List<Usuario> usuarioList;
 
     ListView listView;
     MyAdapter adapter;
-
-
-    //RecyclerView recyclerView;
-    //UsuariosAdapter adapter;
 
 
     @Override
@@ -57,33 +46,16 @@ public class DataInListViewActivity extends AppCompatActivity {
             }
         });
 
-        //textView = (TextView) findViewById(R.id.textview_activity_data_in_list_view);
         progressBar = (ProgressBar) findViewById(R.id.progressbar_activity_data_in_list_view);
         progressBar.setVisibility(View.INVISIBLE);
 
         listView = (ListView) findViewById(R.id.listview_activity_data_in_list_view);
-        //recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
-        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        //linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        //recyclerView.setLayoutManager(linearLayoutManager);
-
-
-
-
-        //textView.setMovementMethod(new ScrollingMovementMethod()); // Para hacer caso al código -> android:scrollbars="vertical"
-
-        // taskList = new ArrayList<>(); // Inicializamos la lista
-
 
     }
 
     public void cargarDatos(){
         adapter = new MyAdapter(getApplicationContext(), usuarioList);
         listView.setAdapter(adapter);
-
-        //adapter = new UsuariosAdapter(getApplicationContext(), usuarioList);
-        //recyclerView.setAdapter(adapter);
-        //recyclerView.setHasFixedSize(true);
 
     }
 
@@ -102,52 +74,20 @@ public class DataInListViewActivity extends AppCompatActivity {
         MyTask task = new MyTask();
         //task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         task.execute(uri);
-        //RequestPackage requestPackage = new RequestPackage();
-        // requestPackage.setMethod("GET");
-        // requestPackage.setMethod("POST");
-        //requestPackage.setMethod(method);
-        //requestPackage.setUri(uri);
-        //requestPackage.setParam("parametro1", "valor1");
-        //requestPackage.setParam("parametro2", "valor2");
-        //requestPackage.setParam("parametro3", "valor3");
-        //requestPackage.setParam("parametro4", "valor4");
-
-        //task.execute(requestPackage);
 
     }
 
-
-
-    // private class MyTask extends AsyncTask<String, String, String>{
     private class MyTask extends AsyncTask<String, String, String> {
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            // cargarDatos("Inicio de carga");
             progressBar.setVisibility(View.VISIBLE);
-
-            /*
-            if(taskList.size() == 0) {
-                progressBar.setVisibility(View.VISIBLE);
-            }
-            taskList.add(this); // Agregando el hilo actual al la lista
-            */
 
         }
 
         @Override
         protected String doInBackground(String... params) { // Recibimos un String. La URL
-                /*
-                for(int i=0; i<=5; i++){
-                    //No podemos cambiar la interface en doInBackground pq es el hilo principal. Por eso podemos usar publishProgress dentro de el
-                    publishProgress("Numero: "+i);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }*/
             String content = HttpManager.getData(params[0]); // Cadena de contenido. Sin seguridad
             return content;
 
@@ -157,39 +97,14 @@ public class DataInListViewActivity extends AppCompatActivity {
         protected void onPostExecute(String result) { // result es el resultado de doInBackground
             super.onPostExecute(result);
             usuarioList = UsuarioJSONParser.parser(result); // PARA PARSEAR EL XML
-
-            /*
-            tasks.remove(this);
-            if(tasks.size() == 0) {
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-            */
-
-            /*
-            if(result == null){
-
-                Toast.makeText(XmlActivity.this, "No se pudo conectar", Toast.LENGTH_SHORT).show();
-                progressBar.setVisibility(View.INVISIBLE);
-                return;
-
-            }
-            */
             cargarDatos();
             progressBar.setVisibility(View.INVISIBLE);
-
-            //cargarDatos(result);
-            /*
-            taskList.remove(this);
-            if(taskList.size() == 0) {
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-            */
 
         }
 
         @Override
         protected void onProgressUpdate(String... values) {
-            //cargarDatos(values[0]);
+            // nothing
         }
     }
 }
